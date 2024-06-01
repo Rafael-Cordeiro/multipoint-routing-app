110d1c57b1da1175fde517ed74fc4c514e085c03
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-05-31 22:02:59 -0300

    [java/core] feature: adiciona método em facade para testar dijkstra com grafo baseado em neo4j

--- logistics-routing-app/src/main/java/dev/rafaelcordeiro/logisticsroutingapp/core/facade/GraphFacade.java
+++ logistics-routing-app/src/main/java/dev/rafaelcordeiro/logisticsroutingapp/core/facade/GraphFacade.java
@@ -4,7 +4,14 @@ import dev.rafaelcordeiro.logisticsroutingapp.core.algorithm.dijkstra.Dijkstra;
 import dev.rafaelcordeiro.logisticsroutingapp.core.dao.GeospatialGraphDAO;
 import dev.rafaelcordeiro.logisticsroutingapp.model.graph.basicgraph.BasicGraph;
 import dev.rafaelcordeiro.logisticsroutingapp.model.graph.basicgraph.BasicGraphNode;
+import dev.rafaelcordeiro.logisticsroutingapp.model.graph.neo4joriented.Node;
+import dev.rafaelcordeiro.logisticsroutingapp.model.tags.OSMIntersection;
+import dev.rafaelcordeiro.logisticsroutingapp.model.tags.OSMRoadSegment;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class GraphFacade {
     private GeospatialGraphDAO geospatialGraphDAO;
 
@@ -22,7 +29,16 @@ public class GraphFacade {
     }
 
     public void testGeoGraphQuery() {
-        geospatialGraphDAO.testGeoGraphQuery();
+        geospatialGraphDAO.getFullGeoGraph();
+    }
+
+    public List<Node<OSMIntersection, OSMRoadSegment>> getRoute(String source, String destination) {
+        var sourceNode = geospatialGraphDAO.getNearestIntersection(source);
+        var destinationNode = geospatialGraphDAO.getNearestIntersection(destination);
+        var graph = geospatialGraphDAO.getFullGeoGraph();
+        Dijkstra dijkstra = new Dijkstra();
+        dijkstra.run(graph, graph.getNodes().get(sourceNode.getData().getOsmid()));
+        return List.of(sourceNode, destinationNode);
     }
 
 }
