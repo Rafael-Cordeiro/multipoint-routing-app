1d36cdcaf825a808621f73e45340777a2b947093
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-07-16 03:49:30 -0300

    [web/components] feature: componente para visualizar rota multiponto ordenada

---
+++ logistics-routing-web/src/components/RoutePipelineView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :class="[index > 0 && index+1 < route.length ? 'q-ml-md' : '', 'q-my-md']" v-for="(section, index) in route" v-bind:key="index">
+    <q-avatar :size="'xs'" :style="styles[index].style" /> {{ section.address.name }}
+  </div>
+</template>
+<script>
+export default {
+  name: 'RoutePipelineView',
+  props: {
+    route: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    styles() {
+      return this.route.map((section, index) => {
+        return {
+          index,
+          style: {
+            "background-color": section.color
+          }
+        }
+      })
+    }
+  }
+}
+</script>
