5edf5342f615853f5e2bed2a403c4895a6ed1907
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-07-16 03:39:59 -0300

    [java/core] add: método estático em Pair para criar instâncias inline

--- logistics-routing-app/src/main/java/dev/rafaelcordeiro/logisticsroutingapp/core/util/Pair.java
+++ logistics-routing-app/src/main/java/dev/rafaelcordeiro/logisticsroutingapp/core/util/Pair.java
@@ -8,4 +8,11 @@ import lombok.Setter;
 public class Pair<L, R> {
     private L left;
     private R right;
+
+    public static <L, R> Pair<L, R> of(L left, R right) {
+        var pair = new Pair<L, R>();
+        pair.left = left;
+        pair.right = right;
+        return pair;
+    }
 }
