86a31d8c91fdc6329345b3db4bbc6f0950209089
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-07-16 03:38:04 -0300

    [java/model] refactor: adapta modelo de requisição de rota e modelo de rota multiponto para constar endereços

--- logistics-routing-app/src/main/java/dev/rafaelcordeiro/logisticsroutingapp/model/api/MultipointRoute.java
+++ logistics-routing-app/src/main/java/dev/rafaelcordeiro/logisticsroutingapp/model/api/MultipointRoute.java
@@ -1,18 +1,20 @@
 package dev.rafaelcordeiro.logisticsroutingapp.model.api;
 
+import dev.rafaelcordeiro.logisticsroutingapp.core.util.Pair;
 import dev.rafaelcordeiro.logisticsroutingapp.model.graph.neo4joriented.Node;
 import dev.rafaelcordeiro.logisticsroutingapp.model.tags.Neo4jTag;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
 @Getter
 @Setter
 public class MultipointRoute<N extends Neo4jTag, R extends Neo4jTag> {
-    private Node<N, R> source;
-    private Node<N, R> destination;
-    private Map<Node<N, R>, LinkedList<Node<N, R>>> paths = new HashMap<>();
+    private Pair<Address, Node<N, R>> source;
+    private Pair<Address, Node<N, R>> destination;
+    private Map<Node<N, R>, Pair<Address, LinkedList<Node<N, R>>>> paths = new LinkedHashMap<>();
 }
--- logistics-routing-app/src/main/java/dev/rafaelcordeiro/logisticsroutingapp/model/api/RouteRequest.java
+++ logistics-routing-app/src/main/java/dev/rafaelcordeiro/logisticsroutingapp/model/api/RouteRequest.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Getter
 @Setter
 public class RouteRequest {
-    private String source;
-    private List<String> intermediates;
-    private String destination;
+    private Address source;
+    private List<Address> intermediates;
+    private Address destination;
 }
