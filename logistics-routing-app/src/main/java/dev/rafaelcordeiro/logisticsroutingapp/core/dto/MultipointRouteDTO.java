83b9fc675cd8501cdeb79edad88ead384dc53cea
Rafael Cordeiro <rafaelcordeiro70@gmail.com>
2024-10-19 05:27:39 -0300

    [core] feature: Adapta DTO para adicionar lista de pontos dos relacionamentos e pontos de nós na estrutura de paths

--- logistics-routing-app/src/main/java/dev/rafaelcordeiro/logisticsroutingapp/core/dto/MultipointRouteDTO.java
+++ logistics-routing-app/src/main/java/dev/rafaelcordeiro/logisticsroutingapp/core/dto/MultipointRouteDTO.java
@@ -4,7 +4,6 @@ import dev.rafaelcordeiro.logisticsroutingapp.core.util.Pair;
 import dev.rafaelcordeiro.logisticsroutingapp.core.util.Triple;
 import dev.rafaelcordeiro.logisticsroutingapp.model.api.Address;
 import dev.rafaelcordeiro.logisticsroutingapp.model.api.MultipointRoute;
-import dev.rafaelcordeiro.logisticsroutingapp.model.graph.neo4joriented.Node;
 import dev.rafaelcordeiro.logisticsroutingapp.model.tags.Neo4jTag;
 import dev.rafaelcordeiro.logisticsroutingapp.model.tags.OSMIntersection;
 import dev.rafaelcordeiro.logisticsroutingapp.model.tags.OSMRoadSegment;
@@ -14,7 +13,6 @@ import org.neo4j.driver.types.Point;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -32,10 +30,14 @@ public class MultipointRouteDTO<N extends Neo4jTag, R extends Neo4jTag> {
             var triple = new Triple<OSMIntersection, Address, ArrayList<Point>>();
             triple.setFirst(node.getData());
             triple.setSecond(path.getLeft());
-            triple.setThird(path.getRight().stream().map(it -> it.getData().getLocation())
-                    .collect(
-                            Collectors.toCollection(ArrayList::new)
-                    ));
+            ArrayList<Point> linestring = new ArrayList<>();
+            var nodes = path.getRight();
+            for (int i = 0; i < path.getRight().size(); i++) {
+                linestring.add(nodes.get(i).getData().getLocation());
+                if (i == path.getRight().size() - 1) break;
+                linestring.addAll(nodes.get(i).getAdjacentNodes().get(nodes.get(i+1)).getData().getGeometry());
+            }
+            triple.setThird(linestring);
             dto.getPaths().add(triple);
         });
         return dto;
